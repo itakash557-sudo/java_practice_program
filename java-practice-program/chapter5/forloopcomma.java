07ad528e532cb587227a93bd85dbdeb6f9f0845f
AKASH S <itakash557@gmail.com>
2026-02-04 23:06:10 +0530

    updated the program

---
+++ java-practice-program/chapter5/forloopcomma.java
@@ -0,0 +1,10 @@
+public class forloopcomma{
+    public static void main(String[]args)
+    {
+        System.out.println("this is for  loop using comma");
+        for(int i=0,j=10;i<=j;i++,j--)  //using comma we can also perform another one task
+        {
+            System.out.println( i +" "+j );
+        }
+    }
+}
