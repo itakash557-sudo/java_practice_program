07c8a05dfd2b5eb7f8f69205ce94673fadb7dbe1
AKASH S <itakash557@gmail.com>
2026-02-04 23:13:40 +0530

    update the program

---
+++ java-practice-program/chapter5/infinityloop.java
@@ -0,0 +1,16 @@
+public class infinityloop{
+    public static void main(String[]args)
+{
+    //    for (;;) 
+    //    {
+    //         System.out.println("Infinite loop");
+    //     }
+
+        for (;true;) 
+        {
+        System.out.println("Infinite loop 2");
+        }
+
+
+}
+}
