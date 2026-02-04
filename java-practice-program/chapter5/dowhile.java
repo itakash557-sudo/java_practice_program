c80b8d4d324aad443c6180c9bc149f7de4288fc5
AKASH S <itakash557@gmail.com>
2026-02-04 22:54:02 +0530

    updated the program

---
+++ java-practice-program/chapter5/dowhile.java
@@ -0,0 +1,14 @@
+    public class dowhile{
+        public static void main(String[]args){
+            System.out.println("welcome");
+        {
+            int a=10;
+            do
+            {
+                System.out.println("it will 1st execute even the condition is false"); //it will print 1st even the condition is false
+            }
+            while(a>11);
+        }       
+            System.out.println("end");
+        }
+    }
