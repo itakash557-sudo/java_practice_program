5f568a1d3f4b9de13b32d333ebf8bcf8bed101c1
AKASH S <itakash557@gmail.com>
2026-02-04 21:24:10 +0530

    Add Java practice program

---
+++ java-practice-program/chapter5/ifelseladder.java
@@ -0,0 +1,20 @@
+class ifelseladder{   //used for multiple condition 
+    // voting system
+    
+    public static void main(String[]args)
+    {
+        System.out.println("if-elseif-else ladder");
+        int age=18;
+        if(age<18){
+            System.out.println("not eligible to vote");
+        }
+        else if(age==18){
+            System.out.println("eligible to vote,welcome adult");
+        }
+        else{
+            System.out.println("eligible to vote");
+        }
+
+    }
+}
+
