0d5b22f0e1809dbf67da0e05e00e30de5ad89b4a
AKASH S <itakash557@gmail.com>
2026-02-05 21:40:03 +0530

    update the program

--- java-practice-program/chapter6/classexample.java
+++ java-practice-program/chapter6/classexample.java
@@ -6,7 +6,7 @@ public class classexample{
             int id; //variable 2
         }
         student st =new student(); //creating a obj 1
-        st.name="Akash";
+        st.name="Akash"; //assigning a value to the variable using object
         st.id=002;
         System.out.println("Name of the Student:"+st.name); //st.name need to but the obj name because u r in a main class it may have name obj
         System.out.println("Student id:"+st.id);
@@ -17,4 +17,5 @@ public class classexample{
         System.out.println("Name of the Student:"+st2.name);
     }
 }
+
 
