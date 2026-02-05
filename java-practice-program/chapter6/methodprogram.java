44a3d8d5486b622d443f3d8684a0b09a8605709f
AKASH S <itakash557@gmail.com>
2026-02-05 21:21:33 +0530

    Updated program

---
+++ java-practice-program/chapter6/methodprogram.java
@@ -0,0 +1,24 @@
+public class methodprogram
+{
+    /* static void add(int a,int b){  //if u use static u don't need to create a object
+            System.out.println(a+b);
+        }
+    public static void main(String []args)
+    {
+        int a=10;
+        int b=20;
+        add(10,20);
+    } */
+//    void sub(int a,int b){      //if u use void then u need to create object
+//     System.out.println(a-b);
+//    }
+//    public static void main(String []args)
+//    {
+//     methodprogram obj=new methodprogram();
+//     int a=10;
+//     int b=20;
+//     obj.sub(10,20);
+//    }
+    
+
+}
