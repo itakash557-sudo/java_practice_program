231e43f2e8aaf6f10a622d3b3c683f16384e2e32
AKASH S <itakash557@gmail.com>
2026-02-06 21:05:55 +0530

    Update program

--- java-practice-program/chapter6/classexample.java
+++ java-practice-program/chapter6/classexample.java
@@ -2,7 +2,7 @@ public class classexample{
     public static void main (String [] args)
     {
         class student{
-            String name; //variable 1
+            String name; //variable 1 
             int id; //variable 2
         }
         student st =new student(); //creating a obj 1
@@ -17,6 +17,7 @@ public class classexample{
         System.out.println("Name of the Student:"+st2.name);
     }
 }
+
 
 
 
