3405ec20d90236fb2c4b36a6fc25d677bcd46951
AKASH S <itakash557@gmail.com>
2026-02-09 21:15:02 +0530

    update the program

--- java-practice-program/chapter6/constructorprogram.java
+++ java-practice-program/chapter6/constructorprogram.java
@@ -5,10 +5,8 @@ public class constructorprogram
         System.out.println("constrctor part"); //when u created the obj it will automatically call the constructor
     }
     public static void main(String[]args)
-    {
-        
+    {        
         constructorprogram obj=new constructorprogram();
-
         
         System.out.println(obj.a); //error: variable a might not have been initialized
     }
