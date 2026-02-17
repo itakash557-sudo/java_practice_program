5441f68f2a879b72e223046f7a233ff38a493898
AKASH S <itakash557@gmail.com>
2026-02-17 20:08:26 +0530

    update the program

--- java-practice-program/chapter5/breakstatement.java
+++ java-practice-program/chapter5/breakstatement.java
@@ -5,7 +5,6 @@ public class breakstatement {
         for (int i = 1; i <= 10; i++) {
             if (i == 5) {
                 break; //stop when condition is true
-
            
             }
             System.out.println(i); 
